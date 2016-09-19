67e5f5c2299b117437bfefbe2ae1dc125e0e688f
Philip Branning <branning@gmail.com>
2016-09-18 17:57:47 -0700

    install miniconda on linux, maybe osx?

---
+++ miniconda_install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# Install conda
+# modified from https://github.com/blaze/odo/blob/master/.travis.yml
+
+miniconda_home=$HOME/miniconda
+
+install_latest()
+{
+  wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda3.sh
+  bash miniconda3.sh -b -p ${miniconda_home}
+  rm miniconda3.sh
+}
+
+install_path()
+{
+  # install miniconda to PATH
+  if ! grep -q miniconda $HOME/.profile
+  then
+    cat <<EOF >>$HOME/.profile
+
+# add conda to path
+if [ -d \$HOME/miniconda ]
+then
+    PATH="\$PATH:${miniconda_home}/bin"
+fi
+EOF
+  fi
+  source $HOME/.profile
+}
+
+# if conda is not on the path, put it there
+# if it's still not on the path, install it
+if ! command -v conda
+then
+  install_path
+  if ! command -v conda
+  then
+    install_latest
+  fi
+fi
+
+conda config --set always_yes yes --set changeps1 no
+conda update conda

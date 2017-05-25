0c947fb1a04f32a4bf7358ecd3bbfa5f13f5ffcd
Philip Branning <branning@gmail.com>
2017-05-25 16:22:23 -0700

    add macos and windows support to yarn install

--- scripts/yarn_install.sh
+++ scripts/yarn_install.sh
@@ -3,14 +3,22 @@
 # install yarn node package manager manager
 # instructions from https://yarnpkg.com/en/docs/install
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-
-repo='https://dl.yarnpkg.com/debian/'
-if ! grep "$repo" /etc/apt/sources.list.d/yarn.list
-then
-  echo "deb ${repo} stable main" \
-    | sudo tee /etc/apt/sources.list.d/yarn.list
-fi
-
-sudo apt update && sudo apt install yarn -y
+case $OSTYPE in
+  darwin*)
+    brew install yarn
+    ;;
+  linux*)
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    repo='https://dl.yarnpkg.com/debian/'
+    if ! grep "$repo" /etc/apt/sources.list.d/yarn.list
+    then
+      echo "deb ${repo} stable main" \
+        | sudo tee /etc/apt/sources.list.d/yarn.list
+    fi
+    sudo apt update && sudo apt install yarn -y
+    ;;
+  cygwin*|msys*|mingw32*)
+    scoop install yarn
+    ;;
+esac
 

a8192e23076283efc686caaec541de67161d3401
Philip Branning <branning@gmail.com>
2017-09-07 22:43:39 +0000

    simple two-liner to install docker using convenience script

---
+++ scripts/docker_install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#
+# use convenience script as described here:
+# https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-using-the-convenience-script
+
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+

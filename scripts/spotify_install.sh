e41ddf7843d520170545d3294ceff62f35ba85a1
Philip Branning <branning@gmail.com>
2017-09-19 18:18:07 -0700

    spotify installation copied from spotify website

---
+++ scripts/spotify_install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# Install Spotify client
+
+error() {
+  echo "`basename $0` $*" >&2
+  exit 1
+}
+
+become_root() {
+  if ! [ $EUID -eq 0 ]
+  then
+    exec sudo /bin/bash "$0" "$@"
+  fi
+}
+
+# if we are being sourced, nothing below here will run
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then return 0; fi
+
+become_root
+
+case $OSTYPE in
+  linux*)
+    # from https://www.spotify.com/us/download/linux/ on 9/19/2017
+    # 1. Add the Spotify repository signing keys to be able to verify downloaded
+    # packages
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886 0DF731E45CE24F27EEEB1450EFDC8610341D9410
+
+    # 2. Add the Spotify repository
+    echo deb http://repository.spotify.com stable non-free \
+      | tee /etc/apt/sources.list.d/spotify.list
+
+    # 3. Update list of available packages
+    apt-get update
+
+    # 4. Install Spotify
+    apt-get install -y spotify-client
+    ;;
+  *) 
+    error "don't know how to install $OSTYPE yet"
+    ;;
+esac
+

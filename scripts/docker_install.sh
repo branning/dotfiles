76e86313beb15a5ff0c56e733574137a9f7d4282
Philip Branning <branning@gmail.com>
2017-11-30 12:39:40 -0800

    cleanup after ourselves by deleting get-docker.sh on exit

--- scripts/docker_install.sh
+++ scripts/docker_install.sh
@@ -3,6 +3,9 @@
 # use convenience script as described here:
 # https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-using-the-convenience-script
 
+set -o errexit
+
+trap 'rm -f get-docker.sh' EXIT
 curl -fsSL get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 

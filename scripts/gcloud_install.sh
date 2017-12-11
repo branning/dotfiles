20827516a5284ddc4a90ec47dc558b78caf4641b
Philip Branning <branning@gmail.com>
2017-12-11 14:51:28 -0800

    update gcloud install script
    - version --> 182
    - install to `/opt` instead of current working directory to keep unversioned files out of $PWD

--- scripts/gcloud_install.sh
+++ scripts/gcloud_install.sh
@@ -10,7 +10,12 @@ info() {
   echo "gcloud install: $*"
 }
 
-version='179.0.0'
+error() {
+  info "$*" >&2
+  exit 1
+}
+
+version='182.0.0'
 
 case $OSTYPE in
   darwin*)
@@ -21,14 +26,36 @@ case $OSTYPE in
     ;;
 esac
 
+has_gcloud(){ command -v gcloud; }
+
+# if we are being sourced, nothing below here will run
+if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then return 0; fi
+
+if false && has_gcloud >/dev/null 2>&1
+then
+  # gcloud already installed, let's update it and exit
+  echo Y | gcloud components update
+  exit 0
+fi
+
+# Download google-cloud-sdk, and automatically cleanup on exit
 url="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${version}-${os}-$(uname -m).tar.gz"
+info "downloading ${url}"
+trap "rm -f $(basename $url)" INT EXIT
 wget -q "$url"
-tar -xzvf $(basename "$url")
-rm $(basename "$url")
-./google-cloud-sdk/install.sh -q --rc-path ~/.profile
+
+# Just add water
+set -x
+destination='/opt/gcloud'
+sudo mkdir -p "$destination"
+sudo chown `id -u`:`id -g` "$destination"
+tar -xzvf $(basename "$url") --directory "$destination"
+"$destination"/google-cloud-sdk/install.sh -q --rc-path ~/.profile
 source ~/.profile
+
+# make sure we install Kubernetes tools
 gcloud components install kubectl -q
 
 # replaces the current shell! exit stage left ... !
-exec -l $SHELL
+#exec -l $SHELL
 

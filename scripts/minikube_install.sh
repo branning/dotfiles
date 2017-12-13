7f347d3145db329a4f32dc086a25b3d27605100a
Philip Branning <branning@gmail.com>
2017-12-13 15:24:52 -0800

    set vm-driver to none if virtualization not supported

--- scripts/minikube_install.sh
+++ scripts/minikube_install.sh
@@ -4,6 +4,10 @@
 
 set -o errexit
 
+info() {
+  echo "`basename $0`: $*"
+}
+
 error() {
   echo >&2 "$*"
   exit 1
@@ -21,3 +25,30 @@ esac
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/"$release"/minikube-"$platform"-amd64 
 chmod +x minikube 
 sudo mv minikube /usr/local/bin/
+
+# check whether virtualization is possible, and set `vm-driver` to `none` if not
+case $platform in
+  linux)
+    if ! grep -m 1 '^flags' /proc/cpuinfo | grep -q -Eo 'vmx|svm'
+    then
+      # the CPU is not capable of virtualization (common on cloud VMs)
+      # so let's run minikube on localhost instead of virtualbox (the default)
+      cpu=$(awk -F: '/^model name/ { print $2; exit 0 }' </proc/cpuinfo)
+      info "Your CPU (${cpu}) does not support virutalization, setting \`vm-driver\` to none"
+      minikube config set vm-driver none
+    else
+      # the kvm-ok tool comes from the cpu-checker Ubuntu package
+      if [[ $(source /etc/os-release; echo $ID) = ubuntu ]]
+      then
+        command -v kvm-ok >/dev/null 2>&1 || $here/scripts/pkg_install.sh cpu-checker
+        if ! kvm-ok
+        then
+          info "virtualization is supported by the CPU, but not enabled in the BIOS. Settings \`vm-driver\` to none"
+          minikube config set vm-driver none
+        fi
+      fi
+    fi
+  darwin) error 'not sure how to set `vm-driver` on macos yet';;
+  *)      error "impossible, did you add platform=${platform} above?";;
+esac
+minikube config set

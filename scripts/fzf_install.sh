6e92580e35ebc8fb4b79339ec751d9d0a222a4a2
Philip Branning <branning@gmail.com>
2017-12-25 13:36:33 -0800

    eliminate existing directory errors for fzf install and quiet output

--- scripts/fzf_install.sh
+++ scripts/fzf_install.sh
@@ -2,8 +2,28 @@
 #
 # Install command line fuzzy finder (fzf) from Github
 
+# ye olde toole shoppe
+library='
+  info
+  quiet
+  '
+for tool in $library
+do
+  here=$(cd $(dirname $BASH_SOURCE[0]); echo $PWD)
+  source "${here}/../library/${tool}.sh"
+done
+here=$(cd $(dirname $BASH_SOURCE[0]); echo $PWD)
+
 if ! command -v fzf >/dev/null 2>&1
 then
-  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-  ~/.fzf/install --all
+  if ! [ -d ~/.fzf ]
+  then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  else
+    quiet pushd ~/.fzf
+    quiet git pull
+    quiet popd
+  fi
+  info "installing fzf"
+  quiet ~/.fzf/install --all
 fi

372c6b3a6c5f62b82d62fbab1f57f5ed0219b522
Philip Branning <branning@gmail.com>
2017-12-25 13:37:30 -0800

    make gitlfs install work on macos

--- scripts/gitlfs_install.sh
+++ scripts/gitlfs_install.sh
@@ -1,15 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Install Git LFS (large file support) via magic
 
-$(cd $(dirname $BASH_SOURCE[0]); echo $PWD)
+here=$(cd $(dirname $BASH_SOURCE[0]); echo $PWD)
 
-# install git-lfs apt repo
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+case $OSTYPE in
+  linux*)
+    (
+    source /etc/os-release
+    if [ "$ID" = debian ] || [ "$ID_LIKE" = debian ]
+    then
+      # install git-lfs apt repo
+      curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+    fi
+    )
+    ;;
+esac
 
 # install git-lfs debian package
 $here/pkg_install.sh git-lfs
-
-#initialize
-git lfs install
-

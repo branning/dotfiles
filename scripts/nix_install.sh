be75f2b3a46b4442f6b40109dfa6ada82ada01e0
Philip Branning <branning@gmail.com>
2026-04-18 13:50:10 -0700

    clean uninstall and test

--- scripts/nix_install.sh
+++ scripts/nix_install.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 #
 
-export NIX_FIRST_BUILD_UID=351
-sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install)
+case "$1" in
+  uninstall|remove)
+    #Uninstall LaunchDaemon org.nixos.nix-daemon
+    sudo launchctl bootout system/org.nixos.nix-daemon
+    sudo rm -f /Library/LaunchDaemons/org.nixos.nix-daemon.plist
+
+    # Delete the files Nix added to your system:
+    sudo rm -rf "/etc/nix" "/var/root/.nix-profile" "/var/root/.nix-defexpr" "/var/root/.nix-channels" "/var/root/.local/state/nix" "/var/root/.cache/nix" "~/.nix-profile" "~/.nix-defexpr" "~/.nix-channels" "~/.local/state/nix" "~/.cache/nix"
+    { mount | grep /nix; } && sudo umount /nix
+    echo "Uninstalled Nix"
+    ;;
+  test)
+    nix-shell -p nix-info --run "nix-info -m"
+    ;;
+  *|install)
+    export NIX_FIRST_BUILD_UID=351
+    sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --yes
+    ;;
+esac
 
